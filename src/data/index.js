dffb9420635103d048be791d3519297b00e4a2ec
SkyIsLimit <63498503+Gagan-D-Singh@users.noreply.github.com>
2022-09-26 11:53:00 +0530

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,74 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Ram',
+    lastName: 'Chandra',
+    email: 'ram@example.com',
+    phone : '9858686549',
+  },
+  {
+    id: 2,
+    firstName: 'Mohd.',
+    lastName: 'Yusuf',
+    email: 'yusuf@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 3,
+    firstName: 'Gurpreet',
+    lastName: 'Singh',
+    email: 'gur@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 4,
+    firstName: 'Rohan',
+    lastName: 'Joshi',
+    email: 'rohan@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 5,
+    firstName: 'Shreeman',
+    lastName: 'Rathore',
+    email: 'shree@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 6,
+    firstName: 'Laurence',
+    lastName: 'Rawat',
+    email: 'laurence@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 7,
+    firstName: 'George',
+    lastName: 'Hook',
+    email: 'george@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 8,
+    firstName: 'Ankita',
+    lastName: 'Sharma',
+    email: 'ankita@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 9,
+    firstName: 'Mathur',
+    lastName: 'Vats',
+    email: 'vats@example.com',
+    phone : '9858654909',
+  },
+  {
+    id: 10,
+    firstName: 'Lakshman',
+    lastName: 'Das',
+    email: 'das@example.com',
+    phone : '9858654909',
+  }
+];
+
+export { employeesData };
